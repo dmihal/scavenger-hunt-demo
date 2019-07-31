2780249081516f47ac382778088df7b44ba0a5a6
David Mihal <david@dmihal.com>
2019-07-31 22:52:10 +0300

    Set up wallet for kovan deployment

--- scavenger-wallet/src/index.js
+++ scavenger-wallet/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BurnerCore from '@burner-wallet/core';
-import { xdai, dai, eth, NativeAsset } from '@burner-wallet/assets';
+import { NativeAsset } from '@burner-wallet/assets';
 import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
-import { InfuraGateway, InjectedGateway, XDaiGateway, HTTPGateway } from '@burner-wallet/core/gateways';
-import Exchange from '@burner-wallet/exchange';
-import { xdaiBridge, uniswapDai } from '@burner-wallet/exchange/pairs';
+import { InfuraGateway, InjectedGateway } from '@burner-wallet/core/gateways';
 import BurnerUI from '@burner-wallet/ui';
 import ScavengerPlugin from '../../scavenger-plugin/ScavengerPlugin';
 
@@ -13,20 +11,16 @@ import ScavengerPlugin from '../../scavenger-plugin/ScavengerPlugin';
 const core = new BurnerCore({
   signers: [new InjectedSigner(), new LocalSigner()],
   gateways: [
-    new HTTPGateway('http://localhost:8545', '5777'),
-    // new InjectedGateway(),
-    // new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
-    // new XDaiGateway(),
+    new InjectedGateway(),
+    new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
   ],
 });
 
 const BurnerWallet = () =>
   <BurnerUI
     core={core}
-    assets={[/*xdai, dai, eth,*/ new NativeAsset({ id: 'teth', name: 'Test Eth', network: '5777' })]}
-    plugins={[new ScavengerPlugin('5777', '0x597370a072AAb1d7cb053aE13bB31bc2E70F6ce1')]}
+    assets={[new NativeAsset({ id: 'keth', name: 'Kovan ETH', network: '42' })]}
+    plugins={[new ScavengerPlugin('42', '0xC5904deefcCeBA55F678728D3f595A4971978649')]}
   />
-
-
 
 ReactDOM.render(<BurnerWallet />, document.getElementById('root'));
